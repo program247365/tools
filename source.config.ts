a50401c6c19081e5f16768e7dee8422a29f1c2c3
Kevin B. Ridgway <k@kbr.sh>
2025-11-04 21:02:19 -0500

    fix: Fix build error

--- source.config.ts
+++ source.config.ts
@@ -1,20 +1,13 @@
-import { defineConfig, defineDocs } from 'fumadocs-mdx/config';
+import { defineConfig, defineDocs, frontmatterSchema } from 'fumadocs-mdx/config';
 import { z } from 'zod';
 
 export const { docs, meta } = defineDocs({
   dir: 'content',
   docs: {
-    schema: (ctx) => {
-      return {
-        ...ctx.schema,
-        frontmatter: z.object({
-          title: z.string(),
-          description: z.string().optional(),
-          tags: z.string().optional(),
-          date: z.string().or(z.date()).optional(),
-        }),
-      };
-    },
+    schema: frontmatterSchema.extend({
+      tags: z.string().optional(),
+      date: z.string().or(z.date()).optional(),
+    }),
   },
 });
 
